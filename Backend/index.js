c5a954044fa5d1bcc5135267128948aa99961592
Akib <akib7599@gmail.com>
2023-03-06 17:38:51 +0530

    worked on api integration

--- Backend/index.js
+++ Backend/index.js
@@ -16,12 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 
-// app.post('/add-category', async (req, res) => {
-
-//     let category = new Category(req.body)
-//     let result = await category.save()
-//     res.send(result)
-// });
+//post api
 
 app.post('/add-category', async (req, res) => {
     const { category } = req.body;
@@ -30,14 +25,9 @@ app.post('/add-category', async (req, res) => {
     res.send(result);
 });
 
+
 //get api
 
-// app.get('/list-category', async (req, res) => {
-
-//     let categorylist = await category.find()
-//     res.send(categorylist)
-
-// })
 app.get('/list-category', async (req, res) => {
     let categorylist = await category.find().sort({ createdAt: -1 });
     res.send(categorylist);
@@ -49,6 +39,20 @@ app.delete("/delete/:_id", async (req, res) => {
     // console.log(req.params)
     let categorylist = await category.deleteOne(req.params)
     res.send(categorylist)
+})
+
+//edit api
+
+app.put("/update-category/:_id",async(req,res)=>{
+
+    //console.log(req.params ,"edit item")
+    let data = await Category.updateOne(
+        req.params,
+        {
+            $set: req.body
+        }
+        );
+    res.send(data)
 })
 
 
